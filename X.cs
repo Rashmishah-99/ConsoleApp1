3bef62025d6214dd29210bc766b9b3768e132dc5
Rashmishah-99 <rs8893905@gmail.com>
2026-06-17 21:47:37 +0530

    Add files via upload

---
+++ X.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class X
+    {
+        public int t;
+
+        public virtual void Display()
+        {
+            Console.WriteLine(t);
+        }
+        public void Show()
+        {
+            Console.WriteLine(t);
+        }
+    }
+    class Y : X
+    {
+        public int u;
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine(u);
+        }
+        public new void Show()
+        {
+            Console.WriteLine(t);
+            Console.WriteLine(u);
+        }
+        public void Test()
+        {
+        }
+    }
+    class MethodOverridingDemo
+    {
+        static void Main(string[] args)
+        {
+            Y obj = new Y();
+            obj.Display();
+            obj.Test();
+
+            X obj2 = new Y();
+            obj2.Display();
+            obj2.Show();
+            //obj2.Test();
+        }
+    }
+}
